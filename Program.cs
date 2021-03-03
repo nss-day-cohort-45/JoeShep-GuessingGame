eac06b04090d201deaea196723696d8326b81dd3
Joe Shepherd <joeshepmedia@gmail.com>
2021-03-03 14:33:41 -0600

    phase 7

--- Program.cs
+++ Program.cs
@@ -9,8 +9,23 @@ int secretNumber = new Random().Next(1, 100);
 
 //Part 1:  Display a message to the user asking them to guess the secret number.
 Console.WriteLine("Try to guess the secret number!");
+// Part 7: Prompt the user for a difficulty level before they are prompted to guess the number.
+Console.WriteLine("Select a difficulty level.");
+Console.WriteLine("1. Easy\n2. Medium\n3. Hard ");
+
+// Convert the incoming user input to a number before saving it
+int level = Int32.Parse(Console.ReadLine());
+
+// Associate the level numbers with the number of guesses to give the player
+Dictionary<int, int> counterCollection = new Dictionary<int, int>() {
+    {1, 8},
+    {2, 6},
+    {3, 4}
+};
+int levelTries = counterCollection[level];
+int counter = levelTries;
 // Phase 3:  Give the user four chances to guess the number.
-for (int i = 1; i <= 4; i++) 
+for (int i = 1; i <= counter; i++) 
 {
      // Part 1: Display a prompt for the user's guess.
     // part 4: Display the number of the user's current guess in the prompt.
@@ -32,10 +47,10 @@ for (int i = 1; i <= 4; i++)
         // part 4: End the loop early if the user guesses the correct number.
         return;
     } 
-    else if (i <= 3)
+    else if (i <= levelTries - 1)
     {
         string hiLow = guessInt > secretNumber ? "high" : "low";
-        Console.WriteLine($"Nope. You guessed too {hiLow}. Try again. You have {4 - i} chance{ (i == 3 ? "" : "s") } left.");
+        Console.WriteLine($"Nope. You guessed too {hiLow}. Try again. You have {levelTries - i} chance{ (i == levelTries - 1 ? "" : "s") } left.");
     }
     else
     {
